5fa8cbc3db28b3ccfd5878e6e69f0cabc63c0e79
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2024-01-15 17:38:48 +0100

    sns icons added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,16 @@
 import { Link } from 'react-router-dom'
 import styles from './styles.module.css'
-// import { GiRotaryPhone } from 'react-icons/gi'
-import { SiMailgun } from 'react-icons/si'
+import { TiSocialLinkedinCircular } from "react-icons/ti";
+import { GrGithub } from "react-icons/gr";
+import { TfiEmail } from "react-icons/tfi";
+import { AiFillMail } from "react-icons/ai";
+import { useState } from 'react';
+import { VscMail } from "react-icons/vsc";
+import { VscMailRead } from "react-icons/vsc";
 
 const Contact = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className={styles.Contact}>
       <div className={styles.layer}>
@@ -15,12 +22,23 @@ const Contact = () => {
             <div className={styles.title}>
               <h2>CONTACT</h2>
             </div>
-            <h3>Merci de me contacter, </h3>
+            <div className={styles.merci}>
+              <div><h3>Merci de me contacter, </h3></div>
+              <div className={styles.link_icons}>
+                <Link to='https://www.linkedin.com/in/hiromi-varnier/' target='_blank' className={styles.linkedin_icons}><TiSocialLinkedinCircular /></Link>
+                <Link to='https://github.com/rominamarmontel/' target='_blank' className={styles.github_icons} ><GrGithub /></Link>
+              </div>
+            </div>
             <div className={styles.description}>
               <div>
                 <div className={styles.contactme}>
-                  <button onClick={() => window.location = 'mailto:romi.varnier@gmail.com'} className={styles.mailme}>
-                    <p>par &nbsp;M<SiMailgun style={{ color: "#c4010a" }} />IL</p>
+                  <button
+                    onClick={() => window.location = 'mailto:romi.varnier@gmail.com'}
+                    className={styles.mailme}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                  >
+                    {isHovered ? <VscMailRead className={styles.mail_icons} /> : <VscMail className={styles.mail_icons} />}
                   </button>
                 </div>
                 <div className={styles.link}>
