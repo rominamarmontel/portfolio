4c6d78c8e7ca7d9117f365d018075da48a8b725d
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2024-01-21 15:43:22 +0100

    works modified

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -34,7 +34,7 @@ const Works = () => {
             <div className={styles.work_image}>
               <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1704827842/Screenshot-2024-01-09-at-20.15.43_nck8jw.jpg' alt='invoice' />
               <div className={styles.mask}>
-                <div className={styles.caption}>Invoicer App<br />Déc 2023<br />Web application</div>
+                <div className={styles.caption}>Invoicer App<br />Déc 2023<br />Web application<br />TypeScript / Next.JS / MongoDB</div>
               </div>
             </div>
           </button>
@@ -46,7 +46,7 @@ const Works = () => {
             <div className={styles.work_image}>
               <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1704823493/Screenshot_2024-01-09_at_19.04.26_cwdvwx.png' alt='imper_next' />
               <div className={styles.mask}>
-                <div className={styles.caption}>Impermanence Films -NextJS ver.-<br />Nov 2023<br />Web application</div>
+                <div className={styles.caption}>Impermanence Films -NextJS ver.-<br />Nov 2023<br />Web application<br />TypeScript / Next.JS / MongoDB</div>
               </div>
             </div>
           </button>
@@ -58,7 +58,7 @@ const Works = () => {
             <div className={styles.work_image}>
               <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1704822762/Screenshot-2024-01-09-at-18.48.06_gvtwzk.jpg' alt='muun' />
               <div className={styles.mask}>
-                <div className={styles.caption}>Muun e-commerce web site<br />Sep 2023<br />e-commerce</div>
+                <div className={styles.caption}>Muun e-commerce web site<br />Sep 2023<br />e-commerce<br />WordPress / Elementor</div>
               </div>
             </div>
           </button>
@@ -70,7 +70,7 @@ const Works = () => {
             <div className={styles.work_image}>
               <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1704822887/Screenshot-2024-01-09-at-18.48.06_acjxri.jpg' alt='impermanencefilms' />
               <div className={styles.mask}>
-                <div className={styles.caption}>Impermanence Films<br />Mai - Juil 2023<br />Web application</div>
+                <div className={styles.caption}>Impermanence Films<br />Mai - Juil 2023<br />Web application<br />React.JS(Vite) / Node.JS(Express.JS) / MongoDB</div>
               </div>
             </div>
           </button>
@@ -83,7 +83,7 @@ const Works = () => {
               {/* <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1693496603/portfolio/247885359-e249cde4-af45-4051-8426-edf21bc77785_1_1_u49f86.png' alt='kanraku' /> */}
               <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1704823834/Screenshot-2024-01-09-at-19.07.49_mgln4l.jpg' alt='kanraku' />
               <div className={styles.mask}>
-                <div className={styles.caption}>KANRAKU HOSAI<br />Mars 2023<br />Projet final@IRONHACK</div>
+                <div className={styles.caption}>KANRAKU HOSAI<br />Mars 2023<br />Projet final@IRONHACK<br />React.JS / Node.JS(Express.JS) / MongoDB</div>
               </div>
             </div>
           </button>
@@ -95,7 +95,7 @@ const Works = () => {
             <div className={styles.work_image}>
               <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1704824104/Screenshot-2024-01-09-at-18.48.06_gaqwet.jpg' alt='exquisite' />
               <div className={styles.mask}>
-                <div className={styles.caption}>Exquisite Story<br />Feb. 2023<br />Équipe projet@IRONHACK</div>
+                <div className={styles.caption}>Exquisite Story<br />Feb. 2023<br />Équipe projet@IRONHACK<br />Node.JS(Express.JS) / Handlebars / MongoDB</div>
               </div>
             </div>
           </button>
@@ -107,7 +107,7 @@ const Works = () => {
             <div className={styles.work_image}>
               <img src='https://res.cloudinary.com/dyu65fpse/image/upload/v1704824479/Screenshot-2024-01-09-at-18.42.31_wlxvcn.jpg' alt='happymonkey' />
               <div className={styles.mask}>
-                <div className={styles.caption}>WIN x WIN HAPPY MONKEY<br />Jan 2023<br />Projet solo@IRON</div>
+                <div className={styles.caption}>WIN x WIN HAPPY MONKEY<br />Jan 2023<br />Projet solo@IRON<br />HTML / CSS / JavaScript</div>
               </div>
             </div>
           </button>
